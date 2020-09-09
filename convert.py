907229475d0b1c479a7a20e2067f3a6ab5fc2fe8
Phil Whelan <phil123@gmail.com>
2020-09-09 12:11:00 -0700

    format code blocks and markdown

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import dateutil.parser
+import html2markdown
 from lxml import etree, html
 
 HTML_DIR = Path('/Users/phil/src/philwhln/philwhln.github.io')
@@ -14,22 +15,44 @@ def main():
 def convert_posts(html_dir, markdown_dir):
     for html_path in html_dir.glob('*.html'):
         slug = Path(html_path).stem
-        print('slug : ' + slug)
         if slug in ['index']:
             continue
+        print('slug : ' + slug)
         convert_file(slug, html_path, markdown_dir)
 
 
 def convert_file(slug, src_path, dst_dir):
     with src_path.open('rb') as f:
         html_content = f.read()
-        tree = html.fromstring(html_content)
-        title = tree.xpath('//title/text()')[0]
-        published_raw = tree.xpath('//abbr[@class="published"]/@title')[0]
-        published = dateutil.parser.parse(published_raw)
-        tags = tree.xpath('//p[@class="entry-meta"]/span[@class="post_tag"]/a[@rel="tag"]/text()')
-        description = tree.xpath('//head/meta[@name="description"]/@content')[0]
-        post_content = tree.xpath('//div[@class="entry-content"]')[0]
+
+    tree = html.fromstring(html_content)
+    title_raw = tree.xpath('//title/text()')[0]
+    title = title_raw.replace(' | Big Fast Blog', '')
+    published_raw = tree.xpath('//abbr[@class="published"]/@title')[0]
+    published = dateutil.parser.parse(published_raw)
+    tags = tree.xpath('//p[@class="entry-meta"]/span[@class="post_tag"]/a[@rel="tag"]/text()')
+    description = tree.xpath('//head/meta[@name="description"]/@content')[0]
+    try:
+        post_elements = tree.xpath('//div[@class="entry-content"]/*')
+    except IndexError:
+        post_elements = tree.xpath('//div[@id="content"]/*')
+
+    markdown_chunks = []
+    for post_element in post_elements:
+        code_elements = post_element.xpath('.//pre')
+        if len(code_elements) > 0:
+            for code_element in code_elements:
+                etree.strip_tags(code_element, 'pre', 'span', 'del', 'small', 'strong')
+                code = code_element.text
+                element_markdown = '```\n' + code + '\n```'
+        else:
+            element_html = etree.tostring(post_element, encoding='unicode', pretty_print=True)
+            element_markdown = html2markdown.convert(element_html)
+
+        markdown_chunks.append(element_markdown)
+
+    post_markdown = str.join('\n\n', markdown_chunks)
+
     dst_filename = str.join('', [
         published.strftime('%Y-%m-%d'),
         '---',
@@ -50,7 +73,7 @@ def convert_file(slug, src_path, dst_dir):
             f.write('  - ' + tag + '\n')
         f.write('description: "' + description + '"\n')
         f.write('---\n')
-        f.write(etree.tostring(post_content, encoding='unicode', pretty_print=True))
+        f.write(post_markdown)
 
 
 if __name__ == '__main__':
